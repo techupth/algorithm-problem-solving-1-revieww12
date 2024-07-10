71a0aba8a13292baf31ff7d091cb68affd88b126
kornkamon view <kornkamon.lert@gmail.com>
2024-07-10 15:06:23 +0700

    feat: add code

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,26 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  //console.log(numbers[0] + numbers[1]);
+  //console.log(numbers[1] + numbers[2]);
+  //console.log(numbers[2] + numbers[3]);
+  let left = 0;
+  let right = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+    if (numbers[i] === numbers[0]) {
+      left = numbers[0];
+      right = left + numbers[1];
+    } else if (numbers[i] === numbers[1]) {
+      left = numbers[1];
+      right = left + numbers[2];
+    } else if (numbers[i] === numbers[2]) {
+      left = numbers[2];
+      right = left + numbers[3];
+    }
+    //console.log(right);
+  }
+  return right;
+}
+const numbers = [2, 7, 11, 15];
+console.log(twoSum(numbers));
